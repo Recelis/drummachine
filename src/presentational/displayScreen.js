943304db53de4faad202b1395d4d39656175e96e
Jacky Lui <jackymlui@live.com>
2019-05-13 14:24:22 +1000

    sound text added to display

--- src/presentational/displayScreen.js
+++ src/presentational/displayScreen.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import Sound from "./sound";
+import { audioclip } from "../audioclip";
 
 class DisplayScreen extends Component {
   render() {
+    let lastValue = "";
+    if (this.props.sequence.length !== 0) {
+      let soundKey = Object.keys(
+        this.props.sequence[this.props.sequence.length - 1]
+      )[0];
+      lastValue = audioclip[soundKey];
+    }
+
+    console.log(lastValue);
     let sequence = this.props.sequence.map((sound, index) => (
-      <Sound sound={sound} index={index} key = {index}
-      prepareToDelete = {this.props.prepareToDelete}
-      deleteThisSound = {this.props.deleteThisSound}
+      <Sound
+        sound={sound}
+        index={index}
+        key={index}
+        prepareToDelete={this.props.prepareToDelete}
+        deleteThisSound={this.props.deleteThisSound}
       />
     ));
-    return <div className="Display-pattern">{sequence}</div>;
+    return (
+      <div className="Display-pattern">
+        <p id="display">{lastValue}</p>
+        {sequence}
+      </div>
+    );
   }
 }
 
